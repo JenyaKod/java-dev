b87e2c12c64b81bb2476eb3e03476b3856b5b72b
JenyaKod <jeka2913@gmail.com>
2023-04-06 22:21:50 +0300

    Change constructor

--- src/main/java/org/com/User.java
+++ src/main/java/org/com/User.java
@@ -5,9 +5,8 @@ public class User {
     private String name;
     private String password;
 
-    public User(String name, String password) {
+    public User(String name) {
         this.name = name;
-        this.password = password;
     }
 
     public String getName() {
