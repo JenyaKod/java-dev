053c2cb8f127a1b5e402a93685f6ced73ec0de84
JenyaKod <jeka2913@gmail.com>
2023-04-06 22:33:06 +0300

    Add user to sout

--- src/main/java/org/com/Main.java
+++ src/main/java/org/com/Main.java
@@ -3,6 +3,7 @@ package org.com;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        User user = new User("Jeck");
+        System.out.println("Hello Java and " + user.getName());
     }
 }
